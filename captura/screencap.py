56bb8d6ed7620521017c735a76224caab09c730b
chy <lunachy@163.com>
2021-01-07 01:47:10 +0800

    1111

--- captura/screencap.py
+++ captura/screencap.py
@@ -18,7 +18,8 @@ with open(CUR_PATH / "config.yaml", encoding="utf-8") as f:
 time_s = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
 
 audio_name = OUT_PATH / f"{time_s}.wav"
-video_tmp_name = OUT_PATH / f"{time_s}_tmp.mp4"
+video_tmp1_name = OUT_PATH / f"{time_s}_tmp1.mp4"
+video_tmp2_name = OUT_PATH / f"{time_s}_tmp2.mp4"
 video_name = OUT_PATH / f"{time_s}.mp4"
 
 p = pyaudio.PyAudio()
@@ -46,18 +47,20 @@ def callback(in_data, frame_count, time_info, status):
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, stream_callback=callback)
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 编码格式
-video = cv2.VideoWriter(str(video_tmp_name), fourcc, cfg["fps"], (cfg["width"], cfg["height"]))
 
-print(f"start recording, wait {cfg['countdown']} seconds!")
-time.sleep(cfg["countdown"])
-stream.start_stream()
-s_time = time.time()
+i = 1
 with mss.mss() as sct:
+    video_tmp1 = cv2.VideoWriter(str(video_tmp1_name), fourcc, cfg["fps"], (cfg["width"], cfg["height"]))
     sct.compression_level = 0
     # Part of the screen to capture
     monitor = {"top": cfg["top"], "left": cfg["left"],
                "width": cfg["width"], "height": cfg["height"], "mon": cfg["mon"]}
     sct.grab(monitor)
+    print(f"start recording, wait {cfg['countdown']} seconds!")
+    time.sleep(cfg["countdown"])
+    stream.start_stream()
+    s_time = time.time()
+
     while True:
         last_time = time.time()
         # Get raw pixels from the screen, save it to a Numpy array
@@ -66,10 +69,15 @@ with mss.mss() as sct:
         # print("fps: {}".format(1.0 / (time.time() - last_time)))
         img_bgra = np.array(img)
         img_bgr = cv2.cvtColor(img_bgra, cv2.COLOR_BGRA2BGR)  # 转为opencv的BGR格式
-        video.write(img_bgr)
+        video_tmp1.write(img_bgr)
 
         if time.time() - s_time > cfg["record_time"]:
             break
+        i += 1
+    video_tmp1.release()
+    print(f"grab {i} images in {cfg['record_time']}s, fps: {i / cfg['record_time']}")
+
+real_fps = i / cfg['record_time']
 
 audio_record_flag = False
 while stream.is_active():
@@ -79,15 +87,20 @@ stream.stop_stream()
 stream.close()
 wf.close()
 p.terminate()
-print("audio recording done!!!!!")
+print("finish recording.")
 
-video.release()
-cv2.destroyAllWindows()
-print("video recording done!!!!!")
+cap = cv2.VideoCapture(str(video_tmp1_name))
+video_tmp2 = cv2.VideoWriter(str(video_tmp2_name), fourcc, real_fps, (cfg["width"], cfg["height"]))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret: break
+    video_tmp2.write(frame)
+video_tmp2.release()
+cap.release()
 
-print("video audio merge!!!!!")
-subprocess.call(f"ffmpeg -i {video_tmp_name} -i {audio_name} {video_name}", shell=True)
+subprocess.call(f"ffmpeg -i {video_tmp2_name} -i {audio_name} {video_name}", shell=True)
 
 if not cfg["debug"]:
     audio_name.unlink()
-    video_tmp_name.unlink()
+    video_tmp1_name.unlink()
+    video_tmp2_name.unlink()
